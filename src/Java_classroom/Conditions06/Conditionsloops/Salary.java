b41000b7bf48bba1c10b5e6b771316007f5a74d4
Mahak Panwar <mahakpanwar333@gmail.com>
2026-07-18 12:00:21 +0530

    My  assignments

---
+++ src/Java_classroom/Conditions06/Conditionsloops/Salary.java
@@ -0,0 +1,13 @@
+package Java_classroom.Conditions06.Conditionsloops;
+
+public class Salary {
+    public static void main(String[] args) {
+        int salary = 25000;
+        if (salary > 4578486) {
+            salary = salary+2000;
+        }else{
+            salary = salary + 1000;
+        }
+        System.out.println(salary);
+    }
+}
