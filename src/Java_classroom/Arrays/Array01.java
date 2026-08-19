ed60d4539fdc7cf8409116f1ac8dca7785c32101
Mahak Panwar <mahakpanwar333@gmail.com>
2026-08-19 09:52:00 +0530

    My  assignments

--- src/Java_classroom/Arrays/Array01.java
+++ src/Java_classroom/Arrays/Array01.java
@@ -1,4 +1,17 @@
 package Java_classroom.Arrays;
 
 public class Array01 {
+    public static void main(String[] args) {
+
+
+
+       // int[] rollno3 = {6,567,4,3,5};
+       // System.out.println(rollno3[2]);
+
+
+        //String[] arr = new String[4];
+        //System.out.println(arr[0]);
+
+
+    }
 }
