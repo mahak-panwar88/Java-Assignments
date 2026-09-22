18a24a4aa2ef9e855ac11201aafd9d00d3f2025b
Mahak Panwar <mahakpanwar333@gmail.com>
2026-09-23 02:54:05 +0530

    My assignment

--- src/Assignment/Methodsandfunctions/ques3.java
+++ src/Assignment/Methodsandfunctions/ques3.java
@@ -1,8 +1,17 @@
 package Assignment.Methodsandfunctions;
 
+import java.util.Scanner;
+
 public class ques3 {
     public static void main(String[] args) {
        // A person is eligible to vote if his/her age is greater than or equal to 18. Define a method to find out if he/she is eligible to vote.
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the age of the person");
+        int n = input.nextInt();
+        if (n>=18){
+            System.out.println("Yes you are allowed to vote");
+        }else{
+            System.out.println("No you are not eligible to vote");
+        }
     }
 }
